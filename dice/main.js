04574392b8131c780d7cad85d60e6c5539aaeb1b
Vasilia Staykova <someonemyfriend@gmail.com>
2023-02-03 01:03:17 +0200

    Add files via upload

--- dice/main.js
+++ dice/main.js
@@ -1,25 +1,46 @@
+// function randomIntFromInterval(min, max) { // min and max included 
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+//   }
+  
+//   const rndInt = randomIntFromInterval(1, 6)
+//   console.log(rndInt);
+
+// function secondRandomIntFromInterval(min, max) { // min and max included 
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+//     }
+  
+//     const secondRndInt = secondRandomIntFromInterval(1, 6)
+//     console.log(secondRndInt);
+
+
+// function thirdRandomIntFromInterval(min, max) { // min and max included 
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+//     }
+    
+//     const thirdRndInt = thirdRandomIntFromInterval(1, 6)
+//     console.log(thirdRndInt);
+
+// let sum = rndInt + secondRndInt + thirdRndInt;
+
+// console.log(sum);
+
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
   
-  const rndInt = randomIntFromInterval(1, 6)
+//   for (var i = 0; i <= 3; i++ ) {
+
+//   }
+
+  const rndInt2 = randomIntFromInterval(1, 6);
+  const rndInt3 = randomIntFromInterval(1, 6);
+  const rndInt = randomIntFromInterval(1, 6);
+
   console.log(rndInt);
-
-function secondRandomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-  
-    const secondRndInt = secondRandomIntFromInterval(1, 6)
-    console.log(secondRndInt);
+  console.log(rndInt2);
+  console.log(rndInt3);
 
 
-function thirdRandomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    
-    const thirdRndInt = thirdRandomIntFromInterval(1, 6)
-    console.log(thirdRndInt);
-
-let sum = rndInt + secondRndInt + thirdRndInt;
+let sum = rndInt + rndInt2 + rndInt3;
 
 console.log(sum);
